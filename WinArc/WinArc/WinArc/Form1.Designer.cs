4c16566b9ba182cd1d74d56a5c542e6a5533cbe2
AlexKovalskyi <Alex.kowalskiy@gmail.com>
2017-01-19 17:53:56 +0200

    Added minor fixes

--- WinArc/WinArc/WinArc/Form1.Designer.cs
+++ WinArc/WinArc/WinArc/Form1.Designer.cs
@@ -28,209 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-            this.splitContainer2.Panel1.SuspendLayout();
-            this.splitContainer2.Panel2.SuspendLayout();
-            this.splitContainer2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
-            this.splitContainer3.Panel1.SuspendLayout();
-            this.splitContainer3.Panel2.SuspendLayout();
-            this.splitContainer3.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(3, 3);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(253, 441);
-            this.treeView1.TabIndex = 0;
-            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(3, 450);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(253, 23);
-            this.progressBar1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Create";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(65, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 50);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Extract";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(127, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(50, 50);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "Help";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(183, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(50, 50);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "Exit";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Location = new System.Drawing.Point(3, 2);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
-            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.listView1);
-            this.splitContainer1.Size = new System.Drawing.Size(739, 476);
-            this.splitContainer1.SplitterDistance = 260;
-            this.splitContainer1.TabIndex = 3;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(4, 3);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(739, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
-            // 
-            // splitContainer2
-            // 
-            this.splitContainer2.Location = new System.Drawing.Point(3, 3);
-            this.splitContainer2.Name = "splitContainer2";
-            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer2.Panel1
-            // 
-            this.splitContainer2.Panel1.Controls.Add(this.splitContainer3);
-            // 
-            // splitContainer2.Panel2
-            // 
-            this.splitContainer2.Panel2.Controls.Add(this.splitContainer1);
-            this.splitContainer2.Size = new System.Drawing.Size(747, 573);
-            this.splitContainer2.SplitterDistance = 85;
-            this.splitContainer2.TabIndex = 4;
-            // 
-            // splitContainer3
-            // 
-            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer3.Name = "splitContainer3";
-            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer3.Panel1
-            // 
-            this.splitContainer3.Panel1.Controls.Add(this.button1);
-            this.splitContainer3.Panel1.Controls.Add(this.button2);
-            this.splitContainer3.Panel1.Controls.Add(this.button4);
-            this.splitContainer3.Panel1.Controls.Add(this.button3);
-            // 
-            // splitContainer3.Panel2
-            // 
-            this.splitContainer3.Panel2.Controls.Add(this.textBox1);
-            this.splitContainer3.Size = new System.Drawing.Size(747, 85);
-            this.splitContainer3.SplitterDistance = 56;
-            this.splitContainer3.TabIndex = 0;
-            // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+			this.splitContainer3.Panel1.SuspendLayout();
+			this.splitContainer3.Panel2.SuspendLayout();
+			this.splitContainer3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(3, 3);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(253, 441);
+			this.treeView1.TabIndex = 0;
+			this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(3, 450);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(253, 23);
+			this.progressBar1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(50, 50);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Create";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.buttonCreate_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(65, 3);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(50, 50);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "Extract";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.buttonExtract_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(127, 3);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(50, 50);
+			this.button3.TabIndex = 0;
+			this.button3.Text = "Help";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(183, 3);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(50, 50);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "Exit";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Location = new System.Drawing.Point(3, 2);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+			this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.listView1);
+			this.splitContainer1.Size = new System.Drawing.Size(739, 476);
+			this.splitContainer1.SplitterDistance = 260;
+			this.splitContainer1.TabIndex = 3;
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3,
             this.columnHeader4});
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(3, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(469, 470);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Name";
-            this.columnHeader1.Width = 132;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Type";
-            this.columnHeader2.Width = 96;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Date modified";
-            this.columnHeader3.Width = 95;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Size";
-            this.columnHeader4.Width = 291;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 591);
-            this.Controls.Add(this.splitContainer2);
-            this.Name = "Form1";
-            this.Text = "WinArc";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-            this.splitContainer2.ResumeLayout(false);
-            this.splitContainer3.Panel1.ResumeLayout(false);
-            this.splitContainer3.Panel2.ResumeLayout(false);
-            this.splitContainer3.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
-            this.splitContainer3.ResumeLayout(false);
-            this.ResumeLayout(false);
+			this.listView1.GridLines = true;
+			this.listView1.Location = new System.Drawing.Point(3, 3);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(469, 470);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.Click += new System.EventHandler(this.listView1_Click);
+			this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Name";
+			this.columnHeader1.Width = 132;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Type";
+			this.columnHeader2.Width = 111;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Date modified";
+			this.columnHeader3.Width = 102;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Size";
+			this.columnHeader4.Width = 291;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(4, 3);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(739, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.Location = new System.Drawing.Point(3, 3);
+			this.splitContainer2.Name = "splitContainer2";
+			this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.Controls.Add(this.splitContainer3);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.Controls.Add(this.splitContainer1);
+			this.splitContainer2.Size = new System.Drawing.Size(747, 573);
+			this.splitContainer2.SplitterDistance = 85;
+			this.splitContainer2.TabIndex = 4;
+			// 
+			// splitContainer3
+			// 
+			this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer3.Name = "splitContainer3";
+			this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer3.Panel1
+			// 
+			this.splitContainer3.Panel1.Controls.Add(this.button1);
+			this.splitContainer3.Panel1.Controls.Add(this.button2);
+			this.splitContainer3.Panel1.Controls.Add(this.button4);
+			this.splitContainer3.Panel1.Controls.Add(this.button3);
+			// 
+			// splitContainer3.Panel2
+			// 
+			this.splitContainer3.Panel2.Controls.Add(this.textBox1);
+			this.splitContainer3.Size = new System.Drawing.Size(747, 85);
+			this.splitContainer3.SplitterDistance = 56;
+			this.splitContainer3.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(760, 591);
+			this.Controls.Add(this.splitContainer2);
+			this.Name = "Form1";
+			this.Text = "WinArc";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+			this.splitContainer2.ResumeLayout(false);
+			this.splitContainer3.Panel1.ResumeLayout(false);
+			this.splitContainer3.Panel2.ResumeLayout(false);
+			this.splitContainer3.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+			this.splitContainer3.ResumeLayout(false);
+			this.ResumeLayout(false);
 
         }
 
--- WinArc/WinArc/WinArc/Form1.cs
+++ WinArc/WinArc/WinArc/Form1.cs
@@ -8,125 +8,153 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinArc.ArchCore;
+using System.Diagnostics;
 
 namespace WinArc
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }        
+	public partial class Form1 : Form
+	{
+		private string path;
+		public Form1()
+		{
+			InitializeComponent();
+		}        
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            DisplayTreeView();
-        }
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			DisplayTreeView();
+		}
 
-        private void DisplayTreeView()
-        {
-            try
-            {
-                string path = "";// I NEEEEED THE PAAAAAAATH !!!!!!!!!!!1111!!!!1
-                TreeNode rootNode;
-                DirectoryInfo mainFolder = new DirectoryInfo(@"D:\trainee");//System.IO.DriveInfo.GetDrives()
-                if (mainFolder.Exists)
-                {
-                    rootNode = new TreeNode(mainFolder.Name);
-                    rootNode.Tag = mainFolder;
-                    GetDirectories(mainFolder.GetDirectories(), rootNode);
-                    treeView1.Nodes.Add(rootNode);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
+		private void DisplayTreeView()
+		{
+			try
+			{
+				string path = "";// I NEEEEED THE PAAAAAAATH !!!!!!!!!!!1111!!!!1
+				TreeNode rootNode;
+				string myComputerPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+				DirectoryInfo mainFolder = new DirectoryInfo(@"D:\Downloads");//System.IO.DriveInfo.GetDrives()[0].RootDirectory();
+				if (mainFolder.Exists)
+				{
+					rootNode = new TreeNode(mainFolder.Name);
+					rootNode.Tag = mainFolder;
+					GetDirectories(mainFolder.GetDirectories(), rootNode);
+					treeView1.Nodes.Add(rootNode);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
 
-        private void GetDirectories(DirectoryInfo[] subDirs,TreeNode nodeToAddTo)
-        {
-            TreeNode aNode;
-            DirectoryInfo[] subSubDirs;
-            foreach (DirectoryInfo subDir in subDirs)
-            {
-                aNode = new TreeNode(subDir.Name, 0, 0);
-                aNode.Tag = subDir;
-                aNode.ImageKey = "folder";
-                subSubDirs = subDir.GetDirectories();
-                if (subSubDirs.Length != 0)
-                {
-                    GetDirectories(subSubDirs, aNode);
-                }
-                nodeToAddTo.Nodes.Add(aNode);
-            }
-        }
+		private void GetDirectories(DirectoryInfo[] subDirs,TreeNode nodeToAddTo)
+		{
+			TreeNode aNode;
+			DirectoryInfo[] subSubDirs;
+			foreach (DirectoryInfo subDir in subDirs)
+			{
+				aNode = new TreeNode(subDir.Name, 0, 0);
+				aNode.Tag = subDir;
+				aNode.ImageKey = "folder";
+				subSubDirs = subDir.GetDirectories();
+				if (subSubDirs.Length != 0)
+				{
+					GetDirectories(subSubDirs, aNode);
+				}
+				nodeToAddTo.Nodes.Add(aNode);
+			}
+		}
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
+		private void button4_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
 
-        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
-        {
-            TreeNode newSelected = e.Node;
-            
-            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
-            DisplayFolderContent(nodeDirInfo);            
-        }
+		private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			TreeNode newSelected = e.Node;
+			path = newSelected.FullPath;
+			textBox1.Text = path;
+			DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+			DisplayFolderContent(nodeDirInfo);            
+		}
 
-        private void textBox1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Return)
-            {
-                //open entered directory on listView1                
-                DirectoryInfo nodeDirInfo = new DirectoryInfo(textBox1.Text);
-                DisplayFolderContent(nodeDirInfo);
-            }            
-        }
+		private void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Return)
+			{
+				//open entered directory on listView1                
+				DirectoryInfo nodeDirInfo = new DirectoryInfo(textBox1.Text);
+				DisplayFolderContent(nodeDirInfo);
+			}            
+		}
 
-        private void DisplayFolderContent(DirectoryInfo nodeDirInfo)
-        {
-            listView1.Items.Clear();
-            ListViewItem.ListViewSubItem[] subItems;
-            ListViewItem item = null;
+		private void DisplayFolderContent(DirectoryInfo nodeDirInfo)
+		{
+			listView1.Items.Clear();
+			ListViewItem.ListViewSubItem[] subItems;
+			ListViewItem item = null;
 
-            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
-            {
-                item = new ListViewItem(dir.Name, 0);
-                subItems = new ListViewItem.ListViewSubItem[]
-                            {
-                                new ListViewItem.ListViewSubItem(item, "Directory"),
-                                new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
-                            };
+			foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+			{
+				item = new ListViewItem(dir.Name, 0);
+				subItems = new ListViewItem.ListViewSubItem[]
+							{
+								new ListViewItem.ListViewSubItem(item, "Directory"),
+								new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
+							};
 
-                item.SubItems.AddRange(subItems);
-                listView1.Items.Add(item);
-            }
-            foreach (FileInfo file in nodeDirInfo.GetFiles())
-            {
-                item = new ListViewItem(file.Name, 1);
-                subItems = new ListViewItem.ListViewSubItem[]
-                          {
-                            new ListViewItem.ListViewSubItem(item, "File"),
-                            new ListViewItem.ListViewSubItem(item, file.LastAccessTime.ToShortDateString()),
-                            new ListViewItem.ListViewSubItem(item, file.Length.ToString())
-                          };
+				item.SubItems.AddRange(subItems);
+				listView1.Items.Add(item);
+			}
+			foreach (FileInfo file in nodeDirInfo.GetFiles())
+			{
+				item = new ListViewItem(file.Name, 1);
+				subItems = new ListViewItem.ListViewSubItem[]
+						  {
+							new ListViewItem.ListViewSubItem(item, "File"),
+							new ListViewItem.ListViewSubItem(item, file.LastAccessTime.ToShortDateString()),
+							new ListViewItem.ListViewSubItem(item, file.Length.ToString())
+						  };
 
-                item.SubItems.AddRange(subItems);
-                listView1.Items.Add(item);
-            }
+				item.SubItems.AddRange(subItems);
+				listView1.Items.Add(item);
+			}
 
-            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
-        }
+			listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+		}
 
-        private void listView1_DoubleClick(object sender, EventArgs e)
-        {
-            //double click on item
-                //- directory : open this directory on listView1
-                //- file : open file
+		private void listView1_DoubleClick(object sender, EventArgs e)
+		{
+			//double click on item
+			//- directory : open this directory on listView1
+			//- file : open file
 
-            //listView1.SelectedItems;
-        }
-    }
+			//listView1.SelectedItems;
+
+
+			listView1.SelectedItems[0].BackColor = Color.Aquamarine;
+
+			//System.Diagnostics.Process.Start(Path.Combine(path,))
+		}
+
+		private void buttonCreate_Click(object sender, EventArgs e)
+		{
+			Archivator arc = new Archivator();
+
+		}
+
+		private void buttonExtract_Click(object sender, EventArgs e)
+		{
+			Archivator arc = new Archivator();
+		}
+
+		private void listView1_Click(object sender, EventArgs e)
+		{
+			string itemName = listView1.SelectedItems[0].ToString().Replace("ListViewItem: {", "");
+			itemName = itemName.Replace("}", "");
+			textBox1.Text = path + "\\" + itemName;
+		}
+	}
 }
